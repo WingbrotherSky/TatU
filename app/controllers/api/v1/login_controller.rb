112ce0851969701cfc710ce30ad9b088ac4bf062
Jacob Tompkins <term171013@gmail.com>
2018-05-22 11:52:35 +0800

    added login and base controllers

---
+++ app/controllers/api/v1/login_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::LoginController < Api::V1::BaseController
+   URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
+
+  def login
+    @user = User.where(openid: wechat_user.fetch('openid')).first_or_initialize
+
+    render json: {
+        userId: @user.id
+      }
+  end
+
+  private
+
+  def wechat_params
+    {
+      appid: ENV['appId'],
+      secret: ENV['appSecret'],
+      js_code: params[:code],
+      grant_type: 'authorization_code'
+    }
+  end
+
+  def wechat_user
+    @wechat_response ||= RestClient.post(URL, wechat_params)
+    @wechat_user ||= JSON.parse(@wechat_response.body)
+  end
+
+end
