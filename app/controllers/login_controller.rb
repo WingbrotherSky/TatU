d3946d46f6a08e8572b00bc7363c8f327d8302b7
Jacob Tompkins <term171013@gmail.com>
2018-05-25 19:22:36 +0800

    fixed email missin bug

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -42,6 +42,9 @@ class LoginController < Api::V1::BaseController
     unless @user.auth_key
       @user.auth_key = SecureRandom.hex(16)
     end
+    unless @user.email
+      @user.email = "#{@user.auth_key}@email.com"
+    end
     if @user.save
       @inbox = Inbox.new()
       @inbox.user = @user
